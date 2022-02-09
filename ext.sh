596a91ca17bb1a3e9156dbbdcbf7a65769c521e4
eggplants <w10776e8w@yahoo.co.jp>
2022-02-09 11:10:19 +0900

    add: ext.sh

---
+++ ext.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if ! [ -f EIJIRO-1448.utf8.TXT ]; then
+  echo "'' is not found." >&2
+  exit 1
+elif ! [ -f wordle_valid_word_dictionary.txt ]; then
+  echo "'' is not found." >&2
+  exit 1
+fi
+n="$(wc -l <wordle_valid_word_dictionary.txt)"
+c=0
+while read i; do
+  echo -n -e "[$c/$n]:$i\r">&2
+  echo -e "${i}\t$(
+    {
+      grep -E "^■${i}  \{[^}]+\} :" EIJIRO-1448.utf8.TXT ||
+      grep -E "^■${i%s}  \{[^}]+\} :" EIJIRO-1448.utf8.TXT ||
+      grep -E "^■${i%es}  \{[^}]+\} :" EIJIRO-1448.utf8.TXT ||
+      grep -E "^■${i%es}  \{[^}]+\} :" EIJIRO-1448.utf8.TXT ||
+      grep -E "^■${i%ed}  \{[^}]+\} :" EIJIRO-1448.utf8.TXT || :
+    } |
+    sed -r 's/^■[^:]+: *//'|tr -d '\r'|sed -z 's/\n/➖/g'|sed 's/➖$//'
+  )"
+  ((c++))
+done < wordle_valid_word_dictionary.txt > wordle_ja.tsv
