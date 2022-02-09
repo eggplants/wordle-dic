f059645650bba835ba06b7261721d87606d7eb17
eggplants <w10776e8w@yahoo.co.jp>
2022-02-10 04:16:44 +0900

    Update ext.sh

--- ext.sh
+++ ext.sh
@@ -3,13 +3,13 @@
 set -euo pipefail
 
 if ! [ -f EIJIRO-1448.utf8.TXT ]; then
-  echo "'' is not found." >&2
+  echo "'EIJIRO-1448.utf8.TXT' is not found." >&2
   exit 1
 elif ! [ -f wordle_valid_word_dictionary.txt ]; then
-  echo "'' is not found." >&2
+  echo "'wordle_valid_word_dictionary.txt' is not found." >&2
   exit 1
 fi
-n="$(wc -l <wordle_valid_word_dictionary.txt)"
+n="$(wc -l < wordle_valid_word_dictionary.txt)"
 c=0
 while read i; do
   echo -n -e "[$c/$n]:$i\r">&2
@@ -21,7 +21,8 @@ while read i; do
       grep -E "^■${i%es}  \{[^}]+\} :" EIJIRO-1448.utf8.TXT ||
       grep -E "^■${i%ed}  \{[^}]+\} :" EIJIRO-1448.utf8.TXT || :
     } |
-    sed -r 's/^■[^:]+: *//'|tr -d '\r'|sed -z 's/\n/➖/g'|sed 's/➖$//'
+    sed -r 's/^■[^:]+: *//' | tr -d '\r' |
+    sed -z 's/\n/➖/g' | sed 's/➖$//'
   )"
   ((c++))
 done < wordle_valid_word_dictionary.txt > wordle_ja.tsv
